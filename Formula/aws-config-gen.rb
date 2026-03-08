964c7025dcdae547cfbec47e233d77a9a24c35fa
khalilkasmi <khalilkasmi1997@gmail.com>
2026-03-08 16:12:17 +0100

    aws-config-gen v0.2.0: Python rewrite

--- Formula/aws-config-gen.rb
+++ Formula/aws-config-gen.rb
@@ -1,19 +1,18 @@
 class AwsConfigGen < Formula
   desc "Interactive TUI to generate AWS SSO config profiles"
   homepage "https://github.com/khalilkasmi/aws-config-gen"
-  url "https://github.com/khalilkasmi/aws-config-gen/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "81de7d7ded5add728ea7bc56a2500bb7ed971a0711e48b406240f870d02e79b6"
+  url "https://github.com/khalilkasmi/aws-config-gen/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "c6d582fe9ca0349569f7296ff18e178becdf75f66c101e88f1707d6281efc8d2"
   license "MIT"
 
-  depends_on "awscli"
+  depends_on "uv"
   depends_on "fzf"
-  depends_on "jq"
 
   def install
     bin.install "aws-config-gen"
   end
 
   test do
-    assert_match "Usage:", shell_output("#{bin}/aws-config-gen --help", 1)
+    assert_match "usage:", shell_output("#{bin}/aws-config-gen --help")
   end
 end
