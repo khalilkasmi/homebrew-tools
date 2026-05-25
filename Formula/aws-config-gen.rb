a0b5db201359d72f6cdf3fe9f4b1098b8c1a50bf
khalilkasmi <khalilkasmi1997@gmail.com>
2026-05-25 11:58:46 +0100

    aws-config-gen v0.4.0

--- Formula/aws-config-gen.rb
+++ Formula/aws-config-gen.rb
@@ -1,8 +1,8 @@
 class AwsConfigGen < Formula
   desc "Interactive TUI to generate AWS SSO config profiles"
   homepage "https://github.com/khalilkasmi/aws-config-gen"
-  url "https://github.com/khalilkasmi/aws-config-gen/archive/refs/tags/v0.3.1.tar.gz"
-  sha256 "d585dabbf21bd2de061c679b499fd589ca0cddefcff48d509b238585812e32e8"
+  url "https://github.com/khalilkasmi/aws-config-gen/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "d680b0d75f38b1cd8fb03a5c91102890256f85128062496474af7e1db3007187"
   license "MIT"
 
   depends_on "uv"
